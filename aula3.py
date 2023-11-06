c03ce9a87acaa8510f1b7e74b4ec61e266ff3484
RenatoRufus <renatooffice1@gmail.com>
2023-11-06 20:49:16 -0300

    .

---
+++ aula3.py
@@ -0,0 +1,25 @@
+""""
+DocString
+Python = linguagem de programação
+Tipo de tipagem = Dinâmica / forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+"""
+#Aula sobre string
+# Aspas simples/duplas
+#Escape
+#r
+
+print('Texto')
+print("Texto \" aspas")
+print(r"Texto \" aspas\"")
+print("expressões regulares")
+print('Texto "aspas"')
+print("Texto 'aspas2'")
+
+print('Um', 'é', 'melhor-implicito-que', sep='-')
+print('Explícito', 'é', 'melhor " do que implícito')
+
+
+
+
