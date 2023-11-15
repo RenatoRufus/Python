192ea17d337c174ea0976cc2017fd4c06c64f82c
RenatoRufus <renatooffice1@gmail.com>
2023-11-14 21:18:53 -0300

    .

---
+++ aula24.py
@@ -0,0 +1,15 @@
+
+nome = 'zero'
+sobrenome = 'Sobrenome Zero'
+idade = 100
+ano_nascimento =  2023 - idade               #"1/1/1923"
+maior_de_idade = idade >= 18
+altura_metros = 1.72
+
+
+print('Nome :', nome)
+print('Sobrenome :', sobrenome)
+print("Idade :",idade)
+print('Ano de nascimento :', ano_nascimento)
+print('É maior de idade? :', maior_de_idade)
+print('Quanto mede a girafa :', altura_metros)
