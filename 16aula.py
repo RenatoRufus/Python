f2222d4f3f4d6a8a7f2c4f605566f907a7aa8e09
RenatoRufus <renatooffice1@gmail.com>
2023-12-09 19:58:25 -0300

    .

---
+++ 16aula.py
@@ -0,0 +1,15 @@
+#operações condicionais
+
+entrada = input('Você quer "entrar" ou "sair"')
+
+if entrada == 'entrar':
+    print('Voce entrou no sistema')
+
+elif entrada == 'sair':
+    print('Voce saiu do sistema')
+
+else:
+    print('Voce não digitou nem entrar e nem sair')
+
+
+print('Fora dos blocos')    
