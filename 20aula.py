5004a262b16892f3caf8265ca0084b67be6041d2
RenatoRufus <renatooffice1@gmail.com>
2023-12-22 19:15:20 -0300

    .

---
+++ 20aula.py
@@ -0,0 +1,18 @@
+primeiro_valor = input('Digite o valor')
+segundo_valor = input('Digite o valor')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor=} é maior ou igual '
+        f' do que {segundo_valor}'
+    )
+else:
+    print(
+        f'{segundo_valor=} é maior '
+        f' do que {primeiro_valor=}'
+    )
+
+
+
+
+
