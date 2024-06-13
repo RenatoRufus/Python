15469d086eb31e774f73f71be6aec0fc048fe426
RenatoRufus <renatooffice1@gmail.com>
2024-06-13 20:55:04 -0300

    .

---
+++ 03.py
@@ -0,0 +1,9 @@
+print(123)
+print('Luiz otávio')
+print("Luiz otávio")
+print("Luiz \"otávio\"")
+print(r"Luiz \"otávio")
+
+print('Luz "Otávio"')
+
+
---
+++ 04.py
@@ -0,0 +1,13 @@
+print(11)
+print(-11)
+print(0)
+
+print(1.1)
+print(1.1, 2.1)
+print( type('olá')     ) #callable
+print( type(1)     ) 
+print( type(1.1)     ) 
+print( type(-1.1)     ) 
+
+
+
