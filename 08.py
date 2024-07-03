5cbaa9070eb42f78e27a5f96372cdd1d004da23a
RenatoRufus <renatooffice1@gmail.com>
2024-07-02 22:01:56 -0300

    .

---
+++ 08.py
@@ -0,0 +1,23 @@
+#pep 8
+#variáveis
+
+nome_completo = 'Renato jr'
+soma_dois_mais_dois = 2 + 2
+int_um = 10
+print(nome_completo)
+print(soma_dois_mais_dois)
+print(soma_dois_mais_dois, nome_completo)
+print(int_um)
+
+nome ='renato'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome: ', nome)
+print('Idade: ',idade)
+print('É maior', maior_de_idade)
+
+
+
+
+
+
---
+++ 09.py
@@ -0,0 +1,15 @@
+nome = 'renato'
+sobrenome = 'jr'
+idade = 20
+ano_nascimento = 2024 - idade
+maior_idade = idade >= 18
+altura = 1.20
+
+print('Nome: ',nome)
+print('sobrenome', sobrenome)
+print('idade',idade)
+print('ano nascimento', ano_nascimento)
+print('Maior de idade', maior_idade)
+print('Altura', altura)
+
+
