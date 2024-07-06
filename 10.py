a6ca36b075f43c12ceaa6b17f0002c24266b17ad
RenatoRufus <renatooffice1@gmail.com>
2024-07-05 23:21:18 -0300

    .

---
+++ 10.py
@@ -0,0 +1,35 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisão',divisao)
+
+divisão_inteira = 10 // 2.2
+print('Divisão inteira', divisão_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+
+modulo = 55 % 2 #resto da divisão
+print('Módulo', modulo)
+
+print( 10 % 8 )
+print(10 % 5 == 0)
+print(10 % 8 == 0)
+
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3* 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
+
+
