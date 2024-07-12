fb86cada86934ebe62d593783d6b7806e2f67ddd
RenatoRufus <renatooffice1@gmail.com>
2024-07-12 20:46:49 -0300

    .

---
+++ 15.py
@@ -0,0 +1,10 @@
+nome = input('Qual o seu nome?')
+print(nome)
+print(f'O seu nome é {nome}')
+
+
+
+numero_1 = input('Digite um número')
+numero_2 = input('Digite outro número')
+
+print(f'A soma dos números é { int(numero_1) + int(numero_2)}')
