d2cb15b663baed5d0d0ff8bb3b9ca06be456a68b
RenatoRufus <renatooffice1@gmail.com>
2024-07-15 20:19:59 -0300

    .

---
+++ 01/17.py
@@ -0,0 +1,10 @@
+#if / elif      / else
+#se / se não se / se não
+entrada = input('Você quer "Entrar" ou "Sair"?')
+
+if entrada == 'entrar':
+    print('Vc entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou "sair" ou "entrar"') 
