c5dc78429ce565a4e365fccba87f2dcc7135c4d9
RenatoRufus <renatooffice1@gmail.com>
2024-07-22 21:12:24 -0300

    .

---
+++ 20.py
@@ -0,0 +1,15 @@
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+senha_permitida = '123456'
+
+if entrada == 'E' and senha_digitada == senha_permitida :
+    print('Entrar')
+else:
+    print('Sair')    
+
+
+print('#############################')
+#avaliação de curto circuito
+print(True and False and True)
+print(True and 0 and True)
+
