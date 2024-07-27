347cc9e1e19b9bc158f406597a4646721c4b1e08
RenatoRufus <renatooffice1@gmail.com>
2024-07-27 19:40:27 -0300

    .

---
+++ 26.py
@@ -0,0 +1,24 @@
+nome = 'Luiz'
+preco = 1000.958976643
+variavel = ''
+print(variavel)
+
+
+"""
+interpolação 
+s - string
+d , i - int
+f - float
+x e X - Hexadecimal ABCDEF0123456789
+
+"""
+
+
+nome2 = 'Luiz'
+preco2 = 1002.453
+variavel2 = '%s, o preço é R$%f' %(nome2, preco2)
+
+print(variavel2)
+print('O hexadecimal de %d é %04x' %(15,15))
+
+
