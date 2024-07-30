e2be7d784547e94713bd513df026cfb3b5cba28a
RenatoRufus <renatooffice1@gmail.com>
2024-07-30 20:33:55 -0300

    .

---
+++ 27.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel:$^10}.')
+print(f'{1000.48439439493943:.1f}')
+print(f'O hexadecimal de 1500 é{1500:08x}')
+print(f'{variavel!r}')
+
+#
+
