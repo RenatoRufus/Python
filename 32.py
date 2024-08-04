e4db6b1c1764a9fb7fdc1a028ec821e2d60722ae
RenatoRufus <renatooffice1@gmail.com>
2024-08-03 21:42:57 -0300

    .

---
+++ 32.py
@@ -0,0 +1,15 @@
+velocidade = 70
+local_carro = 100
+
+RADAR_1 = 60
+LOCAL_1 = 100
+RADAR_RANGE = 1
+
+
+if velocidade > RADAR_1:
+    print('Velocidadde carro passou do radar 1')
+
+if local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+   local_carro <= (LOCAL_1 + RADAR_RANGE) and \
+   velocidade > RADAR_1:
+    print('carro multado em radar 1')                 
