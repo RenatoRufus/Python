2f4884c1731e4eaea98da783380a713d7dbd1adb
RenatoRufus <renatooffice1@gmail.com>
2024-10-10 18:06:54 -0300

    .

---
+++ 36.py
@@ -0,0 +1,17 @@
+"""""
+Flag (bandeira) - Mrca um local
+None = não valor
+is - is not  =  é ou não é (tipo, valor, identidade)
+id = identidade
+"""""
+
+condicao = True
+
+if condicao:
+    passou_no_if = True   #não utilize assim, código ruim
+    print('Faça algo')
+else:
+    passou_no_if = None
+    print('Não faça algo')
+
+print(passou_no_if)        
