3425fbbf59218d7f3454c6bc3202bb97685b7822
RenatoRufus <renatooffice1@gmail.com>
2024-10-17 21:03:12 -0300

    .

---
+++ 57.py
@@ -0,0 +1,20 @@
+"""
+Imutáveis que vimos: str, int, float, bool
+
+"""
+
+
+string = 'rufus Lenhador'
+outra_variavel = f'{string[:3]}ABC'
+#string[3] = 'ABC'
+#print(string[3])
+print(string)
+print(outra_variavel)
+print(string.capitalize())
+print(string.)
+
+
+
+
+
+
