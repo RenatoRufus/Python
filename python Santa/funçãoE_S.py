678b39c44c58b5c41833e660f8c2ff8ce8b3eaa7
RenatoRufus <renatooffice1@gmail.com>
2025-11-25 21:11:51 -0300

    .

---
+++ python Santa/funçãoE_S.py
@@ -0,0 +1,14 @@
+'''
+builtin input 
+'''
+
+nome = input("Informe o seu nome: ")
+print("nome")
+
+nome = "rufus"
+sobrenome = "Carvalho"
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+
+print(nome, sobrenome, sep=" # ")
