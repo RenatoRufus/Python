eeac62713d9da3aa52f5e0be3f38b99ea31deccb
RenatoRufus <renatooffice1@gmail.com>
2025-11-29 20:49:53 -0300

    .

---
+++ RPA/coment.py
@@ -0,0 +1,54 @@
+# Este serve para comentários
+
+"""
+    comentário longo,
+    vaárias linhas 
+
+"""
+
+print("Script executado")
+
+inteiro = 42
+print(inteiro)
+print("Inteiro :", inteiro)
+
+flutuante = 42.0
+print("Flutuante :", flutuante)
+
+complexo = 3 + 4j
+print("Complexos :", complexo)
+
+texto = "Olá Mundo!"
+print("Texto :", texto)
+
+lista = [1, 2, 3, 3] #são mutáveis
+print("Lista :", lista)
+
+tupla = (1,2,3,4,5) #são imutáveis
+print("Tupla :", tupla)
+
+#Conjuntos (set) uma coleção não ordenada de itens unicos
+conjunto = {1, 2, 3, 3}
+print("Conjunto :", conjunto)
+
+
+dicionario = {"Chave" : "valor"}
+print("Dicionário :", dicionario)
+
+
+booleano = True
+print("Boleano :", booleano)
+
+
+#None ausência de valor
+nenhum = None
+print("NoneType (None):", nenhum)
+
+
+#variáveis
+idade = 30
+nome = "Nicole"
+print("Nome: ", nome , ", Idade: ", idade)
+
+
+
